06ffba2fcf90ac17ac9d6ae62a5ca6d148fb276e
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2025-01-18 04:00:45 +0900

    fix: レイアウト修正

--- src/app/componets/top.tsx
+++ src/app/componets/top.tsx
@@ -5,7 +5,9 @@ export default function Top() {
         <div className="flex flex-col gap-24 my-24">
             <section className="flex flex-col gap-4">
                 <h2 className="text-3xl font-bold">■ News</h2>
-                <p><span className="mr-2">2025.1.18</span>By-Production 設立<span className="ml-2 text-red-400 font-bold">New!</span></p>
+                <div className=" ml-24">
+                    <p><span className="mr-2">2025.1.18</span>By-Production 設立<span className="ml-2 text-red-400 font-bold">New!</span></p>
+                </div>
             </section>
             <section className="flex flex-col gap-4">
                 <h2 className="text-3xl font-bold">■ Concept</h2>
@@ -14,8 +16,13 @@ export default function Top() {
             </section>
             <section className="flex flex-col gap-4">
                 <h2 className="text-3xl font-bold">■ Member</h2>
-                <p className="text-md font-bold">MuNi（むに）</p>
-                <p>普段はSWE（ソフトウェアエンジニア）として働きつつ週末に絵を描いている。なんか立ち上げたくね？という軽いノリでサークルを立ち上げるも仲間が居らずぼっちで運営している。<br />比企谷八幡に憧れ色々あって著者と同じ大学学部（明治大学情報コミュニケーション学部）に入る。</p>
+                <div className="flex gap-4">
+                    <Image className="border-2 rounded" src="/MuNi.JPG" width={160} height={160} alt=""/>
+                    <div>
+                        <p className="text-md font-bold">MuNi（むに）</p>
+                        <p className=" leading-8">普段はSWE（ソフトウェアエンジニア）として働きつつ週末に絵を描いている。なんか立ち上げたくね？という軽いノリでサークルを立ち上げるも仲間が居らずぼっちで運営している。<br />比企谷八幡に憧れ色々あって著者と同じ大学学部（明治大学情報コミュニケーション学部）に入る。<br />趣味は漫画、アニメ、バスケ</p>
+                    </div>
+                </div>
                 <div className="ml-2">
                     <h3 className="text-xl font-bold mt-10 mb-4">□ History</h3>
                     <div className="flex flex-col gap-2">
