6da4b2670d5bb62c3cedf25276576a5ce8822d24
Tatsuhiro Motomatsu <tatsuhiro.motomatsu@posse-ap.com>
2025-01-18 17:23:14 +0900

    add: Informationからnavにxロゴを移動

--- src/app/componets/infomation.tsx
+++ src/app/componets/infomation.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function InformationComponents() {
     return (
         <div className="flex flex-col gap-24 my-24">
@@ -11,11 +8,6 @@ export default function InformationComponents() {
             <section className="flex flex-col gap-4">
                 <h2 className="text-3xl font-bold">■ About MuNi</h2>
                 <p>漫画を描きたいずっと。</p>
-                <div>
-                    <Link href={"https://x.com/blaston0130"}>
-                        <Image src="logo/logo-x.svg" width={24} height={24} alt=""/>
-                    </Link>
-                </div>
             </section>
         </div>
     );
--- src/app/shared/header.tsx
+++ src/app/shared/header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -9,12 +10,17 @@ export default function Header() {
     const isInfo = pathname == "/infomation";
     const isWorks = pathname == "/works";
     return (
-        <header className="flex justify-between items-center mx-6 mt-6">
+        <header className="flex justify-between items-center mx-6 mt-6 sticky">
             <div className="text-2xl font-bold"><Link href="/">By-Production</Link></div>
             <ul className="flex gap-10">
                 <li className={`text-xl font-bold hover:opacity-50 ${isTop ? "text-red-400" : ""  }`}><Link href="/">Top</Link></li>
                 <li className={`text-xl font-bold hover:opacity-50 ${isInfo ? "text-red-400" : ""  }`}><Link href="/infomation">Infomation</Link></li>
                 <li className={`text-xl font-bold hover:opacity-50 ${isWorks ? "text-red-400" : "" }`}><Link href="/works">Works</Link></li>
+                <li className="hover:opacity-50">
+                    <Link href={"https://x.com/blaston0130"}>
+                        <Image src="logo/logo-x.svg" width={24} height={24} alt=""/>
+                    </Link>
+                </li>
             </ul>
         </header>
     )
